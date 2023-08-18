b861dffa03d49956df3fe53a34ba5b2bcbc8fe90
Mehmet Ugur Satir <musatir@gmail.com>
2023-08-18 23:21:17 +0200

    limit skip functionalities for pagination

--- controllers/products.js
+++ controllers/products.js
@@ -3,12 +3,12 @@ const Product = require('../models/product');
 const getAllProductsStatic = async (req,res)=>{
     const search = 'woo'
     // throw new Error('testing async errors')
-    const products = await Product.find({}).sort('-name -price')
+    const products = await Product.find({}).sort('name').select('name price').limit(10).skip(5)
     res.status(200).json({products, nbHits:products.length})
 }
 
 const getAllProducts = async (req,res)=>{
-    const {featured, company, name, sort} = req.query;
+    const {featured, company, name, sort, fields} = req.query;
     const queryObject = {};
 
     if(featured){
@@ -32,7 +32,11 @@ const getAllProducts = async (req,res)=>{
     //     sortList = sort.split(',').join(' ')
     // }
 
-    // const products = await Product.find(queryObject).sort(sortList)
+    // if (fields){
+    //     fieldsList = fields.split(',').join(' ')
+    // }
+
+    // const products = await Product.find(queryObject).sort(sortList).select(fieldsList)
 
     // experimental code
   
@@ -45,6 +49,18 @@ const getAllProducts = async (req,res)=>{
     else {
         result = result.sort('createdAt')
     }
+
+    if(fields){
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page-1) * limit
+
+    result = result.skip(skip).limit(limit)
+
     const products = await result
     res.status(200).json({products, nbHits:products.length})
 }
