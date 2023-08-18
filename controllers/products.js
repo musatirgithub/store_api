f7b355015c1b0b3841857d870ca1d78c33087db4
Mehmet Ugur Satir <musatir@gmail.com>
2023-08-18 22:34:08 +0200

    sort function added

--- controllers/products.js
+++ controllers/products.js
@@ -3,12 +3,12 @@ const Product = require('../models/product');
 const getAllProductsStatic = async (req,res)=>{
     const search = 'woo'
     // throw new Error('testing async errors')
-    const products = await Product.find({name : {$regex:search, $options:'i'}})
+    const products = await Product.find({}).sort('-name -price')
     res.status(200).json({products, nbHits:products.length})
 }
 
 const getAllProducts = async (req,res)=>{
-    const {featured, company, name} = req.query;
+    const {featured, company, name, sort} = req.query;
     const queryObject = {};
 
     if(featured){
@@ -24,7 +24,17 @@ const getAllProducts = async (req,res)=>{
     }
 
     console.log(queryObject);
-    const products = await Product.find(queryObject);
+  
+    let result = Product.find(queryObject);
+    if(sort){
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+        console.log(sortList)
+    }
+    else {
+        result = result.sort('createdAt')
+    }
+    const products = await result
     res.status(200).json({products, nbHits:products.length})
 }
 
