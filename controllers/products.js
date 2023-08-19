6a435a65f5ed9ee99cce1efac124e12c9e40b302
Mehmet Ugur Satir <musatir@gmail.com>
2023-08-19 14:56:45 +0200

    static numeric filter

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 const getAllProductsStatic = async (req,res)=>{
     const search = 'woo'
     // throw new Error('testing async errors')
-    const products = await Product.find({}).sort('name').select('name price').limit(10).skip(5)
+    const products = await Product.find({price:{$gt:30}}).sort('price').select('name price company')
     res.status(200).json({products, nbHits:products.length})
 }
 
