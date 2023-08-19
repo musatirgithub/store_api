20d5acb64d74e737dfd0547f83dfd04e20534816
Mehmet Ugur Satir <musatir@gmail.com>
2023-08-19 16:09:22 +0200

    numericFields dynamically added to queryObject

--- controllers/products.js
+++ controllers/products.js
@@ -33,7 +33,16 @@ const getAllProducts = async (req,res)=>{
         }
         const regEx = /\b(<|<=|=|>|>=)\b/g
         let filters = numericFilters.replace(regEx, (match)=>`-${operatorMap[match]}-`)
-        console.log(filters)
+
+        const options = ['price', 'rating']
+        filters = filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split('-')
+            if(options.includes(field)){
+                queryObject[field] = {[operator]: Number(value)}
+            }
+        });
+        console.log(queryObject)
+    
     }
 
     // console.log(queryObject);
